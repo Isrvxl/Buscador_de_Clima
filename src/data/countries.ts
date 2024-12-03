17611ebfd1ccc7c96dd0d963935dabe972688f9c
Isrvxl <isrvelverdugo@gmail.com>
2024-12-03 18:35:14 -0300

    Buscador de Clima Proyect

---
+++ src/data/countries.ts
@@ -0,0 +1,11 @@
+import type { Country } from "../types" 
+
+export const countries : Country[] = [
+    { code: 'ES', name: 'España' },
+    { code: 'US', name: 'Estados Unidos' },
+    { code: 'MX', name: 'México' },
+    { code: 'CO', name: 'Colombia' },
+    { code: 'PE', name: 'Perú' },
+    { code: 'CL', name: 'Chile' },
+    { code: 'AR', name: 'Argentina' }
+]
